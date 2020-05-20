5fb15ab632b42ed83994cfb80f65c65ef4c8fd36
Albert Wu <albertywu@gmail.com>
2020-05-20 16:26:17 -0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 echo "--- installing brew ---"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo "--- installing formulae ---"
+formulae=(
+  autojump
+  fzf
+  readline
+  tldr
+  the_silver_searcher
+)
 
 echo "--- linking config ---"
